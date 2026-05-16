077aab507b472bed4fb96e8dbae253a6b986a3b4
vasanth2081 <er_vasanth@yahoo.co.in>
2026-05-16 04:23:51 +0000

    first commit PredictiveMaintenance-Run-16May-1

--- cap_pred_maintenance/model_building/prep.py
+++ cap_pred_maintenance/model_building/prep.py
@@ -16,16 +16,37 @@ DATASET_PATH = "hf://datasets/v-vasanth2009/capstone-pred-main-vk-12052026/engin
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
-# Handle Missing Values
-for col in df.columns:
-    if df[col].dtype == 'object':
-        df[col].fillna(df[col].mode()[0], inplace=True)
-    else:
-        df[col].fillna(df[col].median(), inplace=True)
-
 # Drop the unnammed : 0 column in dataset
 if 'Unnamed: 0' in df.columns:
     df.drop(columns=['Unnamed: 0'], inplace=True)
+
+def feature_engineering(df):
+
+    df = df.copy()
+
+    df['temp_diff'] = (
+        df['Lub oil temp'] -
+        df['Coolant temp']
+    )
+
+    df['engine_stress'] = (
+        df['Engine rpm'] *
+        df['Coolant temp']
+    )
+
+    df['heat_index'] = (
+        df['Lub oil temp'] +
+        df['Coolant temp']
+    ) / 2
+
+    df['pressure_ratio'] = (
+        df['Fuel pressure'] /
+        (df['Lub oil pressure'] + 1e-5)
+    )
+
+    return df
+
+df = feature_engineering(df)
 
 target_col = 'Engine Condition'
 
@@ -33,10 +54,37 @@ target_col = 'Engine Condition'
 X = df.drop(columns=[target_col])
 y = df[target_col]
 
+# Outlier Handling 
+numerical_cols = X.select_dtypes(
+    include=['int64', 'float64']
+).columns
+
+for col in numerical_cols:
+
+    Q1 = X[col].quantile(0.25)
+    Q3 = X[col].quantile(0.75)
+
+    IQR = Q3 - Q1
+
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    X[col] = X[col].clip(lower, upper)
+    
 # Perform train-test split
 Xtrain, Xtest, ytrain, ytest = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
+
+# Handle Missing Values
+for col in Xtrain.columns:
+    if Xtrain[col].dtype == 'object':
+        fill_value = Xtrain[col].mode()[0]
+    else:
+        fill_value = Xtrain[col].median()
+
+    Xtrain[col] = Xtrain[col].fillna(fill_value)
+    Xtest[col] = Xtest[col].fillna(fill_value)
 
 Xtrain.to_csv("Xtrain.csv",index=False)
 Xtest.to_csv("Xtest.csv",index=False)
